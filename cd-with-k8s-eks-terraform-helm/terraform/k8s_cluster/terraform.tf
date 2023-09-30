8c40207bdde2eb1ab1a8f483951cfb408b3daea7
mai hai <maisonhai3@gmail.com>
2023-09-30 23:06:29 +0700

    Change: use local backend.

--- cd-with-k8s-eks-terraform-helm/terraform/k8s_cluster/terraform.tf
+++ cd-with-k8s-eks-terraform-helm/terraform/k8s_cluster/terraform.tf
@@ -1,11 +1,9 @@
 terraform {
-  backend "s3" {
-    bucket = "httpd-server-terraform-state"
-    key = "cd-retail-web/k8s-cluster/terraform.tfstate"
-    region = "us-east-2"
-
-    dynamodb_table = "terraform_state_locks"
-    encrypt = true
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
   }
 }
 
@@ -18,3 +16,14 @@ provider "aws" {
       }
     }
 }
+
+# terraform {
+#   backend "s3" {
+#     bucket = "httpd-server-terraform-state"
+#     key = "cd-retail-web/k8s-cluster/terraform.tfstate"
+#     region = "us-east-2"
+
+#     dynamodb_table = "terraform_state_locks"
+#     encrypt = true
+#   }
+# }
--- cd-with-k8s-eks-terraform-helm/terraform/k8s_cluster/variables.tf
+++ cd-with-k8s-eks-terraform-helm/terraform/k8s_cluster/variables.tf
@@ -1,6 +1,6 @@
 variable "region_name" {
     type = string
-    default = "us-east-2"
+    default = "us-east-1"
 }
 
 variable "ipam_regions" {
@@ -11,5 +11,5 @@ variable "ipam_regions" {
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
